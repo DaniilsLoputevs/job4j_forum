6ee3ce890db3860b4e1482b37590cf2d6d0b76ca
DaniilsLoputevs <55362320+DaniilsLoputevs@users.noreply.github.com>
2021-01-08 23:31:51 +0000

    1. Spring mock [#348582]

--- src/test/java/ru/job4j/forum/controllers/EditControllerTest.java
+++ src/test/java/ru/job4j/forum/controllers/EditControllerTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.forum.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,7 +13,11 @@ import ru.job4j.forum.ForumApplication;
 import ru.job4j.forum.enity.Post;
 import ru.job4j.forum.services.PostService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = ForumApplication.class)
@@ -34,6 +39,27 @@ class EditControllerTest {
 //                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("/edit"));
+    }
+
+    @Test
+    @WithMockUser
+    void postCreateDTOPost() throws Exception {
+        var requestDTO = Post.builder()
+                .name("Куплю ладу-грант. Дорого. Очень дорого. Ну прям Реально Очень Дорого")
+                .desc("На самом деле это \"Aurus Mr. President\"")
+                .build();
+        this.mockMvc.perform(post("/edit/")
+                .param("createdTime","2020-12-14 19:03:00")
+                .flashAttr("post", requestDTO))
+                .andDo(print())
+            .andExpect(status().is3xxRedirection());
+        ArgumentCaptor<Post> argument = ArgumentCaptor.forClass(Post.class);
+        verify(postService).save(argument.capture());
+
+        assertEquals("Куплю ладу-грант. Дорого. Очень дорого. Ну прям Реально Очень Дорого",
+                argument.getValue().getName());
+        assertEquals("На самом деле это \"Aurus Mr. President\"",
+                argument.getValue().getDesc());
     }
 
     @Test
